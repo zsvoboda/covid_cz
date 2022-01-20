2cf9dc62ee470b93810b2b68845740b43cf2561c
Zdenek Svoboda <zsvoboda@gmail.com>
2022-01-20 11:42:49 +0100

    Ockovani

---
+++ etl/model/dwh/vaccination.sql
@@ -0,0 +1,10 @@
+select (row_number() over ())   as vaccination_id,
+       datum                    as vaccination_date,
+       vakcina                  as vaccination_vaccine,
+       kraj_nuts_kod            as vaccination_county_id,
+       kraj_nuts_kod            as wdf_county_id,
+       vekova_skupina           as vaccination_age_group,
+       prvnich_davek            as vaccination_first_doses,
+       druhych_davek            as vaccination_second_doses,
+       celkem_davek             as vaccination_total_doses
+from stage.ext_ockovani
